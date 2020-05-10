4056b88a8f6d002a73007df5efd797fb51425a6d
Akshay Iyer <akshay.rajagopal@gmail.com>
2020-05-10 18:35:43 -0400

    Added code for min heaps

--- Trees/heap.py
+++ Trees/heap.py
@@ -6,13 +6,17 @@ class MinHeap:
         self.heap = []
 
         if node is not None:
-            self.heap.append(node)
-            self.size += 1
+            if isinstance(node, list):
+                self.heap = node
+                self.size = len(node)
+            else:
+                self.heap.append(node)
+                self.size += 1
 
     def parent(self, pos):
         """Returns position of parent given position of any element"""
         # In a zero-indexed array, parent of any element at i is floor((i-1)/2)
-        return (pos - 1)//2
+        return (pos - 1)//2 if pos > 0 else 0
 
     def left_child(self, pos):
         """Returns position of left child given position of any element"""
@@ -42,28 +46,42 @@ class MinHeap:
         else:
             return False
 
-    def _heapify(self, pos):
+    def _heapify_node(self, pos):
         """
-        Bubbles elements down starting at given position to maintain
-        min heap constraints
+        Heapifies node at given position
         """
         # Only do anything for non-leaf node positions
-        if not self.is_leaf(pos):
+        if not self.is_leaf_node(pos):
             # If parent is greater than either child, it needs
             # to be bubbled down
-            if (self.heap[pos] > self.heap[self.left_child(pos)] or
-                    self.heap[pos] > self.heap[self.right_child(pos)]):
+            left_pos = self.left_child(pos)
+            right_pos = self.right_child(pos)
 
-                # If the left child is less that or equal to right
+            left_child = self.heap[left_pos]
+
+            right_child = self.heap[right_pos] if (
+                right_pos > 0 and right_pos < self.size) else None
+
+            if (self.heap[pos] > left_child or
+                    self.heap[pos] > right_child):
+
+                # If the left child is less than or equal to right
                 # swap parent with left child and recurse
                 # else swap parent with right child and recurse
-                if (self.heap[self.left_child(pos)]
-                        <= self.heap[self.right_child(pos)]):
-                    self.swap(pos, self.left_child(pos))
-                    self._heapify(self.left_child(pos))
+                if right_child is None or (left_child
+                                           <= right_child):
+                    self.swap(pos, left_pos)
+                    self._heapify_node(left_pos)
                 else:
-                    self.swap(pos, self.right_child(pos))
-                    self._heapify(self.right_child(pos))
+                    self.swap(pos, right_pos)
+                    self._heapify_node(right_pos)
+
+    def heapify(self):
+        """
+        Heapifies the current heap
+        """
+        for pos in range((self.size//2)-1, -1, -1):
+            self._heapify_node(pos)
 
     def insert(self, node):
         """Inserts a new node to heap"""
@@ -74,16 +92,21 @@ class MinHeap:
 
         # Bubble up newly inserted node until
         # it is less that it's parent
-        while (self.heap[current_pos] > self.heap[self.parent(current_pos)]):
-            swap(current_pos, self.parent(current_pos))
+        while (self.heap[current_pos] < self.heap[self.parent(current_pos)]):
+            self.swap(current_pos, self.parent(current_pos))
             current_pos = self.parent(current_pos)
 
     def print(self):
         """Prints the heap"""
-        for i in range(0, (self.size/2)):
-            print(f"PARENT: {self.heap[i]}; \
-                    LEFT CHILD: {self.heap[self.left_child(i)]} \
-                    RIGHT CHILD: {self.heap[self.right_child(i)]}")
+        for i in range(0, (self.size//2)):
+            print(
+                f"PARENT: {self.heap[i]}; " +
+                f"LEFT CHILD: {self.heap[self.left_child(i)]}; " +
+                f"RIGHT CHILD: {self.heap[self.right_child(i)]}")
+
+    def extract_min(self):
+        """Returns min node and removes it from heap"""
+        pass
 
 
 class MaxHeap:
--- Trees/test_min_heap.py
+++ Trees/test_min_heap.py
@@ -1,4 +1,6 @@
 import unittest
+import io
+import sys
 
 from . import heap
 
@@ -10,17 +12,64 @@ class TestMinHeap(unittest.TestCase):
     def test_min(self):
         self.assertEqual(15, self.my_heap.min())
 
-    # def test_insert_item(self):
-    #     self.my_heap.insert(15)
-    #     self.my_heap.insert(5)
-    #     self.my_heap.insert(3)
-    #     self.my_heap.insert(17)
-    #     self.my_heap.insert(10)
-    #     self.my_heap.insert(84)
-    #     self.my_heap.insert(19)
-    #     self.my_heap.insert(6)
-    #     self.my_heap.insert(22)
-    #     self.my_heap.insert(9)
+    def test_parent(self):
+        self.assertEqual(2, self.my_heap.parent(5))
+
+    def test_parent_root(self):
+        self.assertEqual(0, self.my_heap.parent(0))
+
+    def test_left_child(self):
+        self.assertEqual(3, self.my_heap.left_child(1))
+
+    def test_left_child_root(self):
+        self.assertEqual(1, self.my_heap.left_child(0))
+
+    def test_right_child(self):
+        self.assertEqual(4, self.my_heap.right_child(1))
+
+    def test_right_child_root(self):
+        self.assertEqual(2, self.my_heap.right_child(0))
+
+    def test_swap(self):
+        my_heap = heap.MinHeap([5, 15, 1])
+        my_heap.swap(1, 2)
+
+        self.assertListEqual([5, 1, 15], my_heap.heap)
+
+    def test_heapify(self):
+        my_heap = heap.MinHeap([5, 15, 1, 2])
+        my_heap.heapify()
+
+        self.assertListEqual([1, 2, 5, 15], my_heap.heap)
+
+    def test_insert_check_min(self):
+        self.my_heap.insert(5)
+        self.my_heap.insert(3)
+        self.my_heap.insert(17)
+        self.my_heap.insert(10)
+        self.my_heap.insert(84)
+        self.my_heap.insert(19)
+        self.my_heap.insert(6)
+        self.my_heap.insert(22)
+        self.my_heap.insert(9)
+
+        self.assertEqual(3, self.my_heap.min())
+
+    def test_insert_check_heap(self):
+        self.my_heap.insert(5)
+        self.my_heap.insert(3)
+        self.my_heap.insert(17)
+        self.my_heap.insert(10)
+
+        capturedOutput = io.StringIO()  # Create StringIO object
+        sys.stdout = capturedOutput     # and redirect stdout.
+        self.my_heap.print()
+        sys.stdout = sys.__stdout__     # Reset redirect.
+
+        self.assertEqual(
+            "PARENT: 3; LEFT CHILD: 10; RIGHT CHILD: 5" + "\n" +
+            "PARENT: 10; LEFT CHILD: 17; RIGHT CHILD: 15" + "\n",
+            capturedOutput.getvalue())
 
 
 if __name__ == "__main__":
